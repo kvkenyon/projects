fd8361a924cc2b25584cf03370b2afc32c2727e9
Kevin Kenyon <kvkenyon@utexas.edu>
2013-10-05 21:14:40 -0500

    Got isosurfaces working

--- assignment1.py
+++ assignment1.py
@@ -9,29 +9,43 @@ reader.ReadAllVectorsOn()
 reader.ReadAllScalarsOn()
 reader.Update()
 
-geom = vtk.vtkImageDataGeometryFilter()
-geom.SetInputConnection(reader.GetOutputPort())
-geom.Update()
+isoValue = 100;
+
+marchingCube = vtk.vtkMarchingCubes()
+marchingCube.SetInputConnection(reader.GetOutputPort())
+marchingCube.SetValue(0,isoValue)
+marchingCube.ComputeNormalsOn()
 
 mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(geom.GetOutputPort())
+mapper.SetInputConnection(marchingCube.GetOutputPort())
+mapper.ScalarVisibilityOff()
 mapper.Update()
 
 actor = vtk.vtkActor()
 actor.SetMapper(mapper)
-actor.RotateY(45)
-
+actor.GetProperty().SetColor(1,1,1)
+actor.GetProperty().SetOpacity(.6)
 
 #create render window and interactor 
 ren = vtk.vtkRenderer()
 renWin = vtk.vtkRenderWindow()
 renWin.AddRenderer(ren)
+renWin.SetSize(600,400)
+renWin.PolygonSmoothingOn()
+
+#Setup interactor
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renWin)
+style = vtk.vtkInteractorStyleTrackballCamera()
+iren.SetInteractorStyle(style)
 
+#Add actor to renderer
+ren.SetBackground(0.329412, 0.34902, 0.427451) #paraview blue from tutorial
 ren.AddActor(actor)
-ren.SetBackground(.3,.6,.3)
 
+
+#Init the interactor and render Start the interactor
+iren.Initialize()
 renWin.Render()
 iren.Start()
 
