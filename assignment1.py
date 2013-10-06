e3c6f7ded8b0f8dbae984d0d0d9bee5071ed165c
Kevin Kenyon <kvkenyon@utexas.edu>
2013-10-06 14:09:58 -0500

    Trying to add class structure to isosurface configuration

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,49 @@
-import Tkinter
 import vtk
-from vtk
-from vtk.util.misc import vtkGetDataRoot
-VTK_DATA_ROOT = vtkGetDataRoot()
+
+class IsoSurfaceConfiguration:
+	def __init__(self, isoValue):
+		self.isoValue = isoValue;
+		#Set default color
+		self.color = (1,1,1)
+		#Set default opacity
+		self.opacity = 1.0
+
+	def setColorR(color):
+		self.color[0] = color
+
+	def setColorG(color):
+		self.color[1] = color
+
+	def setColorB(color):
+		self.color[2] = color
+		
+	def getColor(self):
+		return self.color
+
+	def setOpacity(opacity):
+		self.opacity = opacity
+
+	def getOpacity(self):
+		return self.opacity
+
+	def setIsoValue(isoValue):
+		self.isoValue = isoValue
+
+	def getIsoValue(self):
+		return self.isoValue
+
+	
+
+class VisualizationApp:
+	def __init__(self, fileName):
+		return 0
+		
+
+	def createIsoSurface(isoValue):
+		return 0
+
+	def createCuttingPlane(coordinates):
+		return 0
 
 #create reader
 reader = vtk.vtkStructuredPointsReader()
@@ -22,6 +63,7 @@ mapper = vtk.vtkPolyDataMapper()
 mapper.SetInputConnection(marchingCube.GetOutputPort())
 mapper.ScalarVisibilityOff()
 mapper.Update()
+
 
 actor = vtk.vtkActor()
 actor.SetMapper(mapper)
